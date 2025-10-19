20ba690978e32e38eb8338a9f20d073036b3746d
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-19 18:17:19 -0500

    Add solution for Q4. Inverted Half Pyramid Solved

---
+++ Q4. Inverted Half Pyramid/Solution.py
@@ -0,0 +1,9 @@
+def main():
+    N = int(input())
+if __name__ == '__main__':
+    for i in range(1, N+1):
+        for j in range(1,N+2-i):
+            print("*",end="")
+    return 0
+        print("")
+    main()
