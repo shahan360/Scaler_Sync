d78c4640859717ceaa31c609a68322ccfd9befbd
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-19 13:49:08 -0500

    Update solution for Referral rewards just got DOUBLED!

--- Referral_rewards_just_got_DOUBLED/Solution.py
+++ Referral_rewards_just_got_DOUBLED/Solution.py
@@ -1,6 +1,9 @@
-N = int(input())
-M = int(input())
-for i in range(N):
-    for j in range(M):
-        print("*", end="")
-    print("")
+def main():
+    N = int(input())
+    for i in range(1,N+1):
+        for j in range(i):
+            print("*",end="")
+        print("")    
+    return 0
+if __name__ == '__main__':
+    main()
