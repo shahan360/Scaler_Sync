ba8acd2d350b6a9f55415b5393f0529ebbbe4632
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-19 21:45:19 -0500

    Update solution for Q5. Hollow pyramid pattern Solved

--- Q5. Hollow pyramid pattern/Solution.py
+++ Q5. Hollow pyramid pattern/Solution.py
@@ -1,13 +1,21 @@
-def main():
-    N = int(input())
-    for i in range(N):
-        for j in range(2*N):
-            if j < N - i:
-                print("*", end="")
-            elif j < N - i + 2 * i:
-                print(" ", end="")
-                print("*", end="")                 
-        print("")  # Move to the next line after each row
-    return 0
-if __name__ == '__main__':
-    main()
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    N = int(input())
+    for i in range(N):
+        for j in range(2*N):
+            # Print stars on the left side
+            if j < N - i:
+                print("*", end="")
+            # Print spaces in the middle
+            elif j < N - i + 2 * i:
+                print(" ", end="")
+            # Print stars on the right side
+            else:
+                print("*", end="")                 
+        print("")  # Move to the next line after each row
+    return 0
+
+if __name__ == '__main__':
+    main()
