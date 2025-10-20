6b65c2693a1a8bae5191f7cb2c61f3906aacf482
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-19 22:37:35 -0500

    Delete Day 16 - Beginner: Functions - 1Mon 10 Apr 2023Mandatory0100Class Attendance20  20Problems Solved/Q1. Factorial of N - Functions directory

--- Day 16 - Beginner: Functions - 1Mon 10 Apr 2023Mandatory0100Class Attendance20  20Problems Solved/Q1. Factorial of N - Functions/Solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    # @param A : integer
-    # @return an integer
-    def solve(self, A):
-        fact = 1
-        for i in range(1,A+1):
-            fact = fact*i
-        return fact    
