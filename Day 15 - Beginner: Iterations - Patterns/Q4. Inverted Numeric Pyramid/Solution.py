26b1c9acf436a1b6379c64a27f1685f1f86549f8
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-19 22:35:11 -0500

    Add solution for Q4. Inverted Numeric Pyramid Solved

---
+++ Day 15 - Beginner: Iterations - Patterns/Q4. Inverted Numeric Pyramid/Solution.py
@@ -0,0 +1,11 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    N = int(input())
+    for i in range(N,0,-1):
+        print(" ".join(str(j) for j in range(1, i+1))) 
+    return 0
+
+if __name__ == '__main__':
+    main()
