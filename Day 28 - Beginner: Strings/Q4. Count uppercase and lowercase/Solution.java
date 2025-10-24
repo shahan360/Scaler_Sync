5b20fc817130cccfb8c3e2c06b1475ae7c43c325
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-24 09:07:20 -0500

    Update solution for Q4. Count uppercase and lowercase

--- Day 28 - Beginner: Strings/Q4. Count uppercase and lowercase/Solution.java
+++ Day 28 - Beginner: Strings/Q4. Count uppercase and lowercase/Solution.java
@@ -1,3 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+        int upper = 0;
+        int lower = 0;
+        
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             if (Character.isUpperCase(ch)) {
