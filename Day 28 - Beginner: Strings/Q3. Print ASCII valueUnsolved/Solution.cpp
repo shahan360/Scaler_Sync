2b0d7fcc638c8e56675699130f3fe7bc89ad6527
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-23 23:37:27 -0500

    Update solution for Q3. Print ASCII value Unsolved

--- Day 28 - Beginner: Strings/Q3. Print ASCII valueUnsolved/Solution.cpp
+++ Day 28 - Beginner: Strings/Q3. Print ASCII valueUnsolved/Solution.cpp
@@ -6,14 +6,13 @@ int main()  {
     // YOUR CODE GOES HERE
     // Please take input and print output to standard input/output (stdin/stdout)
     // E.g. 'cin' for input & 'cout' for output
-    
     string str;
     cin >> str;
     
     for (int i = 0; i < str.length(); i++) {
-        cout << int(str[i]);
-        if (i != str.length() - 1) cout << " ";
+        cout << int(str[i]) << " "; // keep one space even after the last ASCII
     }
 
+    // do NOT print endl or \n here
     return 0;
 }
