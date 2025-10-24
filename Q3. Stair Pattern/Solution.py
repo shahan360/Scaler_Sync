21012751add4d2f050ec7ae04ae8eb34d0c60b12
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-24 01:30:21 -0500

    Delete Q3. Stair Pattern directory

--- Q3. Stair Pattern/Solution.py
+++
@@ -1,9 +0,0 @@
-def main():
-    N = int(input())
-    for i in range(1,N+1):
-        for j in range(i):
-            print("*",end="")
-        print("")    
-    return 0
-if __name__ == '__main__':
-    main()
