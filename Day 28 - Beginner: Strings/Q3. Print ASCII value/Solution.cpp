822908b70cf1897f2fc21f57a555177a1d78db52
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-24 20:16:07 -0500

    Update solution for Q3. Print ASCII value

--- Day 28 - Beginner: Strings/Q3. Print ASCII value/Solution.cpp
+++ Day 28 - Beginner: Strings/Q3. Print ASCII value/Solution.cpp
@@ -1,3 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    // YOUR CODE GOES HERE
+    // Please take input and print output to standard input/output (stdin/stdout)
+    // E.g. 'cin' for input & 'cout' for output
+    string str;
+    cin >> str;
     
     for (int i = 0; i < str.length(); i++) {
         cout << int(str[i]) << " "; // keep one space even after the last ASCII
