b06d20d24a11664f0c6cb996936e58fd62d7eba8
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-24 20:13:17 -0500

    Update solution for Q1. Length of String

--- Day 28 - Beginner: Strings/Q1. Length of String/Solution.py
+++ Day 28 - Beginner: Strings/Q1. Length of String/Solution.py
@@ -1,1 +1,11 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    for _ in range(int(input())):
+        print(len(input()))
+
+    return 0
+
+if __name__ == '__main__':
     main()
