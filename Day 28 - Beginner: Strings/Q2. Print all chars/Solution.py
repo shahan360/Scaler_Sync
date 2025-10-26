fde6659b7be96c5fd9aeb0a79249ebcb43028dd6
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-26 01:16:29 -0500

    Add solution for Q2. Print all chars

---
+++ Day 28 - Beginner: Strings/Q2. Print all chars/Solution.py
@@ -0,0 +1,13 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    S = input()
+    for c in S:
+        print(c)
+        # print("")
+
+    return 0
+
+if __name__ == '__main__':
+    main()
