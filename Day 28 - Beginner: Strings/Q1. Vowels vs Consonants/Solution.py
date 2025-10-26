15424932f86855cefa18d422859274360cadc1ba
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-26 16:41:39 -0500

    Update solution for Q1. Vowels vs Consonants

--- Day 28 - Beginner: Strings/Q1. Vowels vs Consonants/Solution.py
+++ Day 28 - Beginner: Strings/Q1. Vowels vs Consonants/Solution.py
@@ -1,28 +1,4 @@
-def main():
-    # YOUR CODE GOES HERE
-    # Please take input and print output to standard input/output (stdin/stdout)
-    # E.g. 'input()/raw_input()' for input & 'print' for output
-    vowels = 0
-    consonants = 0
-
-    t= int(input()) # number of test cases
-
-    for _ in range(t):
-        s = input().lower()
-
-        vowels = 0
-        consonants = 0
-
-        for char in s:
-            if char.isalpha():
-                if char in ('a','e','i','o','u'):
-                    vowels += 1 
-                else:
-                    consonants += 1 
-        
-        print(vowels, consonants)
-
-    return 0
-
-if __name__ == '__main__':
-    main()
+class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
