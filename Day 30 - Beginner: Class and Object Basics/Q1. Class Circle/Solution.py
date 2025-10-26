6e87b449b33a35acd29ce91f4df07cd452bb2e4c
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-26 18:16:52 -0500

    Add solution for Q1. Class Circle

---
+++ Day 30 - Beginner: Class and Object Basics/Q1. Class Circle/Solution.py
@@ -0,0 +1,23 @@
+import math
+class Circle:
+    # Define properties here
+    
+    
+    # Define constructor here
+    def __init__(self,r):
+        self.r  = r
+
+
+    def perimeter(self):
+        # Complete the function
+        return 2*3.14*r
+    
+    
+    def area(self):
+        # Complete the function
+        return 3.14*(r**2)
+    
+        
+# a = new Circle(3)  // Radius = 3
+# a.perimeter() // 18.84
+# a.area() // 28.26
