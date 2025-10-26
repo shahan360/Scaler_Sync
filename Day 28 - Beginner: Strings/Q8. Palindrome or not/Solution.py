22e8dc29d264f9e37ac2f8cba07aeb10a6329c33
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-26 16:20:36 -0500

    Update solution for Q8. Palindrome or not

--- Day 28 - Beginner: Strings/Q8. Palindrome or not/Solution.py
+++ Day 28 - Beginner: Strings/Q8. Palindrome or not/Solution.py
@@ -1,15 +1,4 @@
-def isPalindrome(s):
-    '''Input: s takes the string as input
-       Output:return the True or False'''
-    # YOUR CODE GOES HERE
-    start = 0
-    end = len(s) - 1
-    while start < end:
-        if s[start] != s[end]:
-            return False
-        start+=1
-        end-=1
-    return True
-
-    
-
+class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
