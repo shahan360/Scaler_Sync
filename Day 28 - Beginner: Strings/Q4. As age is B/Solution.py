5964ff4829428d9bab43a0c2a90536ac81dc1599
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-26 17:07:57 -0500

    Add solution for Q4. A's age is B

---
+++ Day 28 - Beginner: Strings/Q4. As age is B/Solution.py
@@ -0,0 +1,11 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    A, B = input().split()
+    print(f"{A}'s age is {B}") 
+
+    return 0
+
+if __name__ == '__main__':
+    main()
