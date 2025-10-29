43b88b247e262321a2d4f8b3c0c5c9fe9d9f0111
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-29 05:09:44 -0500

    Add solution for Q3. Copy the Array

---
+++ Day 20 - Beginner: 1D Array - 1/Q3. Copy the Array/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int[] solve(final int[] A, int B) {
+        int outArr[] = new int[A.length];
+        for(int i=0;i<A.length;i++){
+            outArr[i]=A[i]+B;
+        }
+
+        return outArr;
+    }
+}
