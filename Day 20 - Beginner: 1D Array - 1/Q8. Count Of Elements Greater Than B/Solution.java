2700ddf93f82c74a02208bfbf2271a9636195cd7
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-10-29 05:24:49 -0500

    Update solution for Q8. Count Of Elements Greater Than B

--- Day 20 - Beginner: 1D Array - 1/Q8. Count Of Elements Greater Than B/Solution.java
+++ Day 20 - Beginner: 1D Array - 1/Q8. Count Of Elements Greater Than B/Solution.java
@@ -1,12 +1,23 @@
-public class Solution {
-    public int solve(int[] A, int B) {
-        int N = A.length;
-        int count=0;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner scn = new Scanner(System.in);
+        int N = scn.nextInt();
+        int A[] = new int[N];
         for(int i=0;i<N;i++){
-            if(A[i]>B){
-                count++;
+            A[i]=scn.nextInt();
+        }
+        for(int i=0;i<A.length;i++){
+            if(A[i]<0){
+                System.out.print(A[i]+" ");
             }
         }
-        return count;
+        
     }
 }
