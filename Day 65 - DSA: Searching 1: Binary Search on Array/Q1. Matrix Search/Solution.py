987c11ebe6e85bc08a0327ea0eddb4885175271a
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-11-01 19:48:08 -0500

    Update solution for Q1. Matrix Search

--- Day 65 - DSA: Searching 1: Binary Search on Array/Q1. Matrix Search/Solution.py
+++ Day 65 - DSA: Searching 1: Binary Search on Array/Q1. Matrix Search/Solution.py
@@ -3,8 +3,26 @@ class Solution:
     # @param B : integer
     # @return an integer
     def searchMatrix(self, A, B):
-        for row in A:
-            for val in row:
-                if val == B:
-                    return 1
+        # Brute Force Approach
+        # for row in A:
+        #     for val in row:
+        #         if val == B:
+        #             return 1
+        # return 0
+
+        # Binary Search Approach
+        N = len(A)
+        M = len(A[0])
+        low, high = 0, N * M - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            row = mid // M 
+            col = mid % M 
+            if A[row][col] == B:
+                return 1 
+            elif A[row][col] < B:
+                low = mid + 1
+            else:
+                high = mid - 1
         return 0
