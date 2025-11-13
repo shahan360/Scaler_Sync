17043fc136e127ed5a8a5719d8f0bae2fd0e363c
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-11-12 22:45:32 -0600

    Add solution for Q1. Evaluate Expression

---
+++ Day 70 - DSA: Stacks/Q1. Evaluate Expression/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+	# @param A : list of strings
+	# @return an integer
+	def evalRPN(self, A):
+        stack = []
+        for token in A:
+            if token not in "+-*/":
+                stack.append(int(token))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                if token == '+':
+                    stack.append(a + b)
+                elif token == '-':
+                    stack.append(a - b)
+                elif token == '*':
+                    stack.append(a * b)
+                elif token == '/':
+                    stack.append(int(a / b))
+        return stack[0]
