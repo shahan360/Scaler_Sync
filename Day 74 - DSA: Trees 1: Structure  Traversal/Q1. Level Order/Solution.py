ce7a7e8c66c9b0df4af1f001095687052555ac7d
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-11-13 22:03:36 -0600

    Add solution for Q1. Level Order

---
+++ Day 74 - DSA: Trees 1: Structure  Traversal/Q1. Level Order/Solution.py
@@ -0,0 +1,32 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
+
+class Solution:
+    # @param A : root node of tree
+    # @return a list of list of integers
+    def solve(self, A):
+        if not A:
+            return []
+        
+        queue = [A]
+        result = []
+
+        while queue:
+            level_size = len(queue)
+            current_level = []
+
+            for _ in range(level_size):
+                node = queue.pop(0)
+                current_level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            result.append(current_level)
+        
+        return result
