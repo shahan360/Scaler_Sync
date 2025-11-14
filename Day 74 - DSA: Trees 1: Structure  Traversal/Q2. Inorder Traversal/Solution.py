c2636b18cb6291dea858668694db858d5fd1654a
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-11-13 22:12:14 -0600

    Add solution for Q2. Inorder Traversal

---
+++ Day 74 - DSA: Trees 1: Structure  Traversal/Q2. Inorder Traversal/Solution.py
@@ -0,0 +1,23 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#	def __init__(self, x):
+#		self.val = x
+#		self.left = None
+#		self.right = None
+
+class Solution:
+	# @param A : root node of tree
+	# @return a list of integers
+	def inorderTraversal(self, A):
+        result = []
+
+        def inorder(node):
+            if not node:
+                return 
+            
+            inorder(node.left)
+            result.append(node.val)
+            inorder(node.right)
+        
+        inorder(A)
+        return result
