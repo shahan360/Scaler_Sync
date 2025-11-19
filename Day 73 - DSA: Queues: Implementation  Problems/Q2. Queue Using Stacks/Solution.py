ba8975bd9b7d95e047f73a220e269e5947dea2c0
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-11-18 20:52:17 -0600

    Add solution for Q2. Queue Using Stacks

---
+++ Day 73 - DSA: Queues: Implementation  Problems/Q2. Queue Using Stacks/Solution.py
@@ -0,0 +1,45 @@
+class UserQueue:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack_in = []
+        self.stack_out = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack_in.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.stack_in and not self.stack_out
+
+
+# Your UserQueue object will be instantiated and called as such:
+# obj = UserQueue()
+# obj.push(x)
+# param2 = obj.pop()
+# param3 = obj.peek()
+# param4 = obj.empty()
