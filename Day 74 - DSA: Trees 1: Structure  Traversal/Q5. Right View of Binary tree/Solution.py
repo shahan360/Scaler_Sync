fe1711c39e90209ccf6d232d07d77e147c62ed1e
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-11-23 02:36:56 -0600

    Add solution for Q5. Right View of Binary tree

---
+++ Day 74 - DSA: Trees 1: Structure  Traversal/Q5. Right View of Binary tree/Solution.py
@@ -0,0 +1,31 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
+
+class Solution:
+    # @param A : root node of tree
+    # @return a list of integers
+    def solve(self, A):
+        if not A:
+            return []
+        
+        result = []
+        queue = [A]
+
+        while queue:
+            level_size = len(queue)
+            rightmost = None
+            for _ in range(level_size):
+                node = queue.pop(0)
+                rightmost = node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(rightmost)
+        
+        return result
