67432c180172e00c66472f46f9a2906f1903d689
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-11-23 01:41:13 -0600

    Add solution for Q4. Deserialize Binary Tree

---
+++ Day 74 - DSA: Trees 1: Structure  Traversal/Q4. Deserialize Binary Tree/Solution.py
@@ -0,0 +1,39 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
+
+class Solution:
+    # @param A : list of integers
+    # @return the root node in the tree
+    def solve(self, A):
+        if not A:
+            return None 
+        if A[0] == -1:
+            return None 
+        
+        root = TreeNode(A[0])
+        queue = [root]
+        i = 1
+
+        while queue and i < len(A):
+            current = queue.pop(0)
+
+            # Assign left child
+            if i < len(A):
+                if A[i] != -1:
+                    current.left = TreeNode(A[i])
+                    queue.append(current.left)
+                i += 1
+
+            # Assign right child
+            if i < len(A):
+                if A[i] != -1:
+                    current.right = TreeNode(A[i])
+                    queue.append(current.right)
+                i += 1
+        
+        return root
+
