a281d144df21053125b5c0d58c6a01895ed29149
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2025-11-22 20:42:20 -0600

    Add solution for Q3. Serialize Binary Tree

---
+++ Day 74 - DSA: Trees 1: Structure  Traversal/Q3. Serialize Binary Tree/Solution.py
@@ -0,0 +1,27 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
+
+class Solution:
+    # @param A : root node of tree
+    # @return a list of integers
+    def solve(self, A):
+        if not A:
+            return []
+        
+        queue = [A]
+        result = []
+
+        while queue:
+            node = queue.pop(0)
+            if node:
+                result.append(node.val)
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                result.append(-1)
+
+        return result
